80a70e163937d8095af27cfbdda85b4e869f284d
Kris Carta <kriscarta@gmail.com>
2014-09-15 11:25:43 +0200

    Add Exercise_10_05

---
+++ Exercise_10_05/src/Person.java
@@ -0,0 +1,30 @@
+
+public class Person
+{
+   // Make a Person class
+   // With a name and a birthday
+   // And manually-defined getters and setters
+   
+   private String name;
+   private String birthday;
+   
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+   
+   public String getName()
+   {
+      return this.name;
+   }
+
+   public void setBirthday(String birthday)
+   {
+      this.birthday = birthday;
+   }
+   
+   public String getBirthday()
+   {
+      return this.birthday;
+   }
+}
