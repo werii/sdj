54151881a5f3ee4ce811d6d691aba1857d8a6113
Kris Carta <kriscarta@gmail.com>
2014-10-02 11:15:19 +0200

    Add Exercise_16_06

---
+++ Exercise_16_06/src/Circle.java
@@ -0,0 +1,51 @@
+
+public class Circle
+{
+   // Declare fields
+   private double radius;
+   private Point center;
+
+   // Constructor
+   public Circle(Point center, double radius)
+   {
+      this.radius = radius;
+      // Give a new center instance to each new circle object
+      this.center = center.copy();
+   }
+   
+   // Another constructor
+   // This takes in x and y coordinates instead of just a Point object
+   public Circle(double x, double y, double radius)
+   {
+      this.radius = radius;
+      // Makes a reference to a new internal Point object
+      this.center = new Point(x, y);
+   }
+
+   // Getters
+   public Point getCenter()
+   {
+      // Get a copy of the center object
+      // This way the caller only gets access to a copy, not the original
+      return this.center.copy();
+   }
+   
+   public double getRadius()
+   {
+      return this.radius;
+   }
+
+   // Move the center
+   // Use the method in the Point object
+   public void moveTo(double x, double y)
+   {
+      this.center.moveTo(x, y);
+      
+   }
+   
+   // Return a String representation
+   public String toString()
+   {
+      return "Radius: " + this.radius + ", Center: " + this.center.toString();
+   }
+}
