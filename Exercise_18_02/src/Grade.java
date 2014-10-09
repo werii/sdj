c816bb9873d640fe0a57af254325c641ab525ed1
Kris Carta <kriscarta@gmail.com>
2014-10-09 09:51:03 +0200

    Add Exercise_18_02a

---
+++ Exercise_18_02/src/Grade.java
@@ -0,0 +1,43 @@
+
+public class Grade
+{
+   private String courseName;
+   private int grade;
+   
+   public Grade(String courseName, int grade)
+   {
+      // First error: courseName wasn't initialized here
+      this.courseName = courseName; // Fixed
+      
+      // Second error: "grade = this.grade;" was backwards
+      this.grade = grade; // Fixed
+   }
+   
+   public String getCourseName()
+   {
+      // Not an error, but I like to use this.
+      // when referring to class variables
+      return courseName;
+   }
+   
+   public int getGrade()
+   {
+      // Not an error, but I like to use this.
+      // when referring to class variables
+      return grade;
+   }
+   
+   public void setGrade(int grade)
+   {
+      // Third error: same as the second, "grade = this.grade;" is backwards
+      this.grade = grade; // Fixed
+   }
+   
+   // Fourth error: ToString() should be toString() 
+   public String toString() // Fixed
+   {
+      return this.courseName + ", " + this.grade;
+      
+   }
+
+}
---
+++ Exercise_18_02/src/GradeListTest.java
@@ -0,0 +1,20 @@
+
+public class GradeListTest
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("// Test class Grade");
+      System.out.println("Create a Grade object with \"SDJ\" and grade = -3");
+      Grade grade1 = new Grade("SDJ", -3);
+      System.out.println("Course name = " + grade1.getCourseName());
+      System.out.println("Grade = " + grade1.getGrade());
+      System.out.println("Method toString(): \n" + grade1);
+      System.out.println("Set grade to 12");
+      grade1.setGrade(12);
+      System.out.println("Grade = " + grade1.getGrade());
+      System.out.println("Method toString(): \n" + grade1);
+
+   }
+
+}
